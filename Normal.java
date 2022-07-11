6284fd6d04259eec29b387529d013db0dec54fa4
Avinash Maske <85235293+avinash201120@users.noreply.github.com>
2022-07-12 00:42:03 +0530

    Add files via upload

---
+++ Normal.java
@@ -0,0 +1,7 @@
+public class Normal {
+    public static void main(String args[]){
+        System.out.println("Cheshmesh");
+        System.out.println("Cheshmesh");
+        System.out.println("Cheshmesh");
+    }
+}
---
+++ myNormal.java
@@ -0,0 +1,12 @@
+public class myNormal {
+    public static void main(String args[]){
+        String name1="Cheshmesh";
+        String name2="Aagav";
+        String name3="Sweet";
+
+        System.out.println(name1+" Vaishnavi");
+        System.out.println(name2+" Vaishnavi");
+        System.out.println(name3+" Vaishnavi");
+
+    }
+}
