e1cf8ae078053fdc6cebfbfe7c1a2932e9337c49
Avinash Maske <85235293+avinash201120@users.noreply.github.com>
2022-07-12 00:30:47 +0530

    Add files via upload

---
+++ myMethod.java
@@ -0,0 +1,11 @@
+public class myMethod {
+    static void myMethod(String name){
+        System.out.println(name + " Vaishnavi");
+    }
+
+    public static void main(String args[]){
+        myMethod("Cheshmesh");
+        myMethod("Aagav");
+        myMethod("Sweet");
+    }
+}
