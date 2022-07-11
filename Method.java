8eb0555a354b4caf7eb0a23fedcaf2d44177aa67
Avinash Maske <85235293+avinash201120@users.noreply.github.com>
2022-07-12 00:29:58 +0530

    Add files via upload

---
+++ Method.java
@@ -0,0 +1,11 @@
+public class Method {
+    static void Method(){
+        System.out.println("Cheshmesh");
+    }
+
+    public static void main(String args[]){
+        Method();
+        Method();
+        Method();
+    }
+}
